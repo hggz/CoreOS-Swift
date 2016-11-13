526865eb30f8e4683fe2888312fef51bb4347f0b
Hugo Gonzalez <hugo.gonzales@mirpartners.com>
2016-11-12 16:25:57 -0800

    Initial Commit

---
+++ CoreOS/CoreOS.h
@@ -0,0 +1,19 @@
+//
+//  CoreOS.h
+//  CoreOS
+//
+//  Created by hugogonzalez on 11/12/16.
+//  Copyright © 2016 Cat Bakery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreOS.
+FOUNDATION_EXPORT double CoreOSVersionNumber;
+
+//! Project version string for CoreOS.
+FOUNDATION_EXPORT const unsigned char CoreOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreOS/PublicHeader.h>
+
+
